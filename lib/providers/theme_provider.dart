0052d7f355870b1e38d2f43aa4212faaf4cf3abb
bothost-source <destinyjob493@gmail.com>
2026-06-02 17:54:53 +0100

    Update theme_provider.dart

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,16 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  bool _isDarkMode = true;
-  bool get isDarkMode => _isDarkMode;
+  ThemeMode _themeMode = ThemeMode.dark;
 
-  void toggleTheme() {
-    _isDarkMode = !_isDarkMode;
+  ThemeMode get themeMode => _themeMode;
+
+  bool get isDarkMode => _themeMode == ThemeMode.dark;
+  bool get isLightMode => _themeMode == ThemeMode.light;
+  bool get isSystemMode => _themeMode == ThemeMode.system;
+
+  ThemeProvider() {
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedTheme = prefs.getString('theme_mode') ?? 'dark';
+
+    switch (savedTheme) {
+      case 'light':
+        _themeMode = ThemeMode.light;
+        break;
+      case 'system':
+        _themeMode = ThemeMode.system;
+        break;
+      default:
+        _themeMode = ThemeMode.dark;
+    }
     notifyListeners();
   }
 
-  void setDarkMode(bool value) {
-    _isDarkMode = value;
+  Future<void> setThemeMode(ThemeMode mode) async {
+    _themeMode = mode;
+    final prefs = await SharedPreferences.getInstance();
+
+    String modeString;
+    switch (mode) {
+      case ThemeMode.light:
+        modeString = 'light';
+        break;
+      case ThemeMode.system:
+        modeString = 'system';
+        break;
+      case ThemeMode.dark:
+        modeString = 'dark';
+        break;
+    }
+
+    await prefs.setString('theme_mode', modeString);
     notifyListeners();
+  }
+
+  Future<void> toggleTheme() async {
+    if (_themeMode == ThemeMode.dark) {
+      await setThemeMode(ThemeMode.light);
+    } else {
+      await setThemeMode(ThemeMode.dark);
+    }
+  }
+
+  Future<void> setLightMode() async {
+    await setThemeMode(ThemeMode.light);
+  }
+
+  Future<void> setDarkMode() async {
+    await setThemeMode(ThemeMode.dark);
+  }
+
+  Future<void> setSystemMode() async {
+    await setThemeMode(ThemeMode.system);
   }
 }
