38c22b5b834d72a1b596a7d0e7d70cea553e69e5
bothost-source <destinyjob493@gmail.com>
2026-06-03 08:52:13 +0100

    Delete lib/services/audio_recorder.dart

--- lib/services/audio_recorder.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:async';
-
-abstract class AudioRecorderInterface {
-  Future<bool> hasPermission();
-  Future<void> start(String path);
-  Future<String?> stop();
-  Future<void> dispose();
-}
