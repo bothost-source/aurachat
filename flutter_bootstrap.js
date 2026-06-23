90151782595c9a43c6e2d3d2da9d530782b0cd7e
bothost-source <bothost-source@users.noreply.github.com>
2026-06-23 19:59:28 +0000

    deploy: 9140b9b8a37a8663a86faebbd55741c3131b8945

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3508229682"
+    serviceWorkerVersion: "3617913035"
   }
 });
