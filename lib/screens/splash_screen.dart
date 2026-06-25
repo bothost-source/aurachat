8d41a993a4c9c6565de8b99e57e7c45eadf1b939
bothost-source <destinyjob493@gmail.com>
2026-06-25 19:29:52 +0100

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../../providers/auth_provider.dart';
+import '../../providers/auth_provider.dart' show AuraAuthProvider;
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
