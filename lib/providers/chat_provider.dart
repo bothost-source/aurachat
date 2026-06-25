68beade1d9b2505f61842bf9409b07697064085c
bothost-source <destinyjob493@gmail.com>
2026-06-25 23:45:22 +0100

    Update chat_provider.dart

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:cloud_firestore/cloud_firestore.dart';
