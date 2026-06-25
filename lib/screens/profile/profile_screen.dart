d2388e410782995660d795dfeb060200ec53ee88
bothost-source <destinyjob493@gmail.com>
2026-06-25 19:31:02 +0100

    Update profile_screen.dart

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import '../../providers/auth_provider.dart';
+import '../../providers/auth_provider.dart' show AuraAuthProvider;
 import '../../providers/settings_provider.dart';
 
 class ProfileScreen extends StatefulWidget {
