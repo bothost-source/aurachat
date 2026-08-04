fdd452b295dc8524b2502bb0b0f4648493afd1d8
bothost-source <destinyjob493@gmail.com>
2026-08-04 23:45:59 +0100

    Update auth.js

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 const { body, validationResult } = require('express-validator');
 
 // Generate JWT
