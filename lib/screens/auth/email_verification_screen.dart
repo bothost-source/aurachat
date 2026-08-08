19d2f1535666d0a8e9f45b79b8b78b5434e1917d
bothost-source <destinyjob493@gmail.com>
2026-08-08 06:15:32 +0100

    Update email_verification_screen.dart

--- lib/screens/auth/email_verification_screen.dart
+++ lib/screens/auth/email_verification_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
